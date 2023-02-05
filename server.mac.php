aa99613760a6136c1992125209974b88fbd31f29
Michael Staatz <michael@staatzstreich.de>
2023-02-05 10:55:21 +0100

    Reworked script for mac os

--- server.mac.php
+++ server.mac.php
@@ -1,102 +1,111 @@
 #!/usr/bin/env php
 
 <?php
-// Defaults
-$debug = true;
-$script = basename(__FILE__);
-$current_path = __DIR__;
-$nproc = trim(`sysctl -n hw.ncpu`);
-$default_interface = '127.0.0.1';
-$default_port = 8000;
-$delete_fake_index = false;
 
-// User settings
-if ($argc >= 2) {
-    $network_access = explode(':', escapeshellarg($argv[1]));
-    if (count($network_access) === 2) {
-        $user_interface = $network_access[0];
-        $user_port = (int)$network_access[1];
-    } else {
-        $user_port = (int)str_replace("'", "", escapeshellarg($argv[1]));
+// Check for min. PHP Version
+PHP_VERSION_ID > 80100 ?: die('You need PHP >= 8.1.0 to run this script') . PHP_EOL;
+
+// Data Transfer Object for overall configuration
+class ConfigurationDTO
+{
+    public static string $shortOptions = 'i::p::n::d::h::';
+    public static array $longOptions = ['interface::', 'port::', 'cores::', 'directory::', 'help::'];
+
+    public function __construct(
+        public readonly bool $debug = true,
+        public readonly string $script = '',
+        public readonly ?string $currentPath = '',
+        public readonly string $numberOfCores = '',
+        public readonly string $host = '127.0.0.1',
+        public readonly int $port = 8000,
+        public readonly bool $deleteFakeIndex = false,
+        public readonly string $pathToPhp = '',
+    ) {
     }
 }
+
+// Parse commandline
+$options = getopt(ConfigurationDTO::$shortOptions, ConfigurationDTO::$longOptions);
+
+// Create configuration based on commandline parameters
+$config = new ConfigurationDTO(
+    script: basename(__FILE__),
+    currentPath: $options['d'] ?? $options['directory'] ??= '',
+    numberOfCores: $options['n'] ?? $options['cores'] ??= trim(`sysctl -n hw.ncpu`),
+    host: $options['i'] ?? $options['interface'] ??= '127.0.0.1',
+    port: $options['p'] ?? $options['port'] ??= 8000,
+    pathToPhp: trim(`which php`),
+);
 
 // Functions
-function create_fake_index($path = null)
+function create_fake_index(string $path = null): void
 {
-    if (is_null($path)) {
-        $path = __DIR__;
-    }
+    $path = $path === null || empty($path) ? __DIR__ : $path;
+
     if (!file_exists($path . '/index.php') && !file_exists($path . '/index.html')) {
         file_put_contents($path . '/index.php', '<?php phpinfo(); ?>');
+        file_put_contents($path . '/index.html', '');
     }
 }
-function run_server()
-{
-    global $default_interface,
-        $default_port,
-        $user_interface,
-        $user_port,
-        $nproc,
-        $argc,
-        $argv;
 
+function run_server(ConfigurationDTO $config): void
+{
     echo 'Starting web server...' . PHP_EOL;
     echo 'Press [Ctrl + C] to stop it.' . PHP_EOL;
     echo PHP_EOL;
-    if ($argc === 3) {
-        pcntl_exec(
-            trim(`which php`),
-            ['-S', (isset($user_interface) ? $user_interface : $default_interface) . ':' . (isset($user_port) ? $user_port : $default_port), '-t', $argv[2]],
-            ['PHP_CLI_SERVER_WORKERS' => $nproc]
-        );
-    } else {
-        pcntl_exec(
-            trim(`which php`),
-            ['-S', (isset($user_interface) ? $user_interface : $default_interface) . ':' . (isset($user_port) ? $user_port : $default_port)],
-            ['PHP_CLI_SERVER_WORKERS' => $nproc]
-        );
+
+    $arguments = ['-S', $config->host . ':' . $config->port];
+    if (empty($config->currentPath) === false) {
+        $subarguments = ['-t', $config->currentPath];
+        array_push($arguments, ...$subarguments);
     }
+
+    pcntl_exec(
+        trim(`which php`),
+        $arguments,
+        ['PHP_CLI_SERVER_WORKERS' => $config->numberOfCores]
+    );
 }
 
 // Display
-echo $script . ' - Self PHP Web Server' . PHP_EOL . PHP_EOL;
-if ($argc >= 2 && ($argv[1] === '-h' || $argv[1] === '--help')) {
-    echo ' - Usage: ' . $script . ' [interface:port] [/path/to/serve]' . PHP_EOL . PHP_EOL;
+echo $config->script . ' - Self PHP Web Server' . PHP_EOL . PHP_EOL;
+if ((isset($options['h']) && $options['h'] === false) || (isset($options['help']) && $options['help'] === false)) {
+    echo " - Usage: $config->script [options]" . PHP_EOL . PHP_EOL;
+    echo "Options:" . PHP_EOL;
+    echo "\t-h, --help\t\tDisplay help for the given command." . PHP_EOL;
+    echo "\t-i, --interface\t\tIP - Adress for server." . PHP_EOL;
+    echo "\t-p, --port\t\tPort for server." . PHP_EOL;
+    echo "\t-c, --cores\t\tNumber of processor - cores to use." . PHP_EOL;
+    echo "\t-d, --directory\t\tPublic folder for server." . PHP_EOL;
+    echo PHP_EOL;
     echo ' - System Info:' . PHP_EOL;
-    echo "\t" . '- Current Path: ' . $current_path . PHP_EOL;
-    echo "\t" . '- CPU Cores: ' . $nproc . PHP_EOL;
-    echo "\t" . '- OS: ' . PHP_OS . PHP_EOL;
-    echo "\t" . '- PHP: ' . PHP_VERSION . PHP_EOL;
+    echo "\t - Current Path: $config->currentPath" . PHP_EOL;
+    echo "\t - CPU Cores:  $config->numberOfCores" . PHP_EOL;
+    echo "\t - OS: " . PHP_OS . PHP_EOL;
+    echo "\t - PHP: " . PHP_VERSION . PHP_EOL;
     echo PHP_EOL;
     exit(1);
 }
-if ($debug === true) {
+
+if ($config->debug === true) {
     echo ' - Debug:' . PHP_EOL;
-    echo "\t" . ' - Args: ' . implode(',', $argv) . PHP_EOL;
-    echo "\t" . ' - Count: ' . $argc . PHP_EOL;
-    echo "\t" . ' - Env: PHP_CLI_SERVER_WORKERS=' . $nproc . PHP_EOL;
-    if ($argc === 3) {
-        echo "\t" . ' - Command: ' . trim(`which php`) . ' -S ' . (isset($user_interface) ? $user_interface : $default_interface) . ':' . (isset($user_port) ? $user_port : $default_port) . ' -t ' . $argv[2] . PHP_EOL;
+    echo "\t - Args   : " . implode(',', $argv) . PHP_EOL;
+    echo "\t - Count  : $argc" . PHP_EOL;
+    echo "\t - Env    : PHP_CLI_SERVER_WORKERS=$config->numberOfCores" . PHP_EOL;
+    if (empty($config->currentPath) === false) {
+        echo "\t - Command: $config->pathToPhp -S $config->host:$config->port -t $config->currentPath" . PHP_EOL;
     } else {
-        echo "\t" . ' - Command: ' . trim(`which php`) . ' -S ' . (isset($user_interface) ? $user_interface : $default_interface) . ':' . (isset($user_port) ? $user_port : $default_port) . PHP_EOL;
+        echo "\t - Command: $config->pathToPhp -S $config->host:$config->port" . PHP_EOL;
     }
     echo PHP_EOL;
 }
 
 // Check if web root or document is given
-if ($argc >= 2) {
-    // If nothing is given, create a fake index file
-    if ($argc === 3) {
-        create_fake_index($argv[2]);
-    }
-    if ($argc === 2) {
-        create_fake_index();
-    }
-}
+// If nothing is given, create a fake index file
+create_fake_index($config->currentPath);
 
 // Run web server
 echo ' - Output:' . PHP_EOL;
 echo PHP_EOL;
-run_server();
+run_server($config);
 echo PHP_EOL;
